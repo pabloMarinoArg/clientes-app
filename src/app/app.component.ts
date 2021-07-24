6ab300f6a90febadbcf29f2cc999aa51c030ddca
pabloMarinoArg <31575175+pabloMarinoArg@users.noreply.github.com>
2021-07-24 01:25:52 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clientes-app';
+  nombre:string="Pablo";
+  apellido:string="Marino";
+  curso:string="Angular"
 }
